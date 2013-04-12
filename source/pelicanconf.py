1517fbc0ee1eeb3d8c2d00933307a5346913b53c
gabriell nascimento <gabriellhrn@gmail.com>
2013-04-12 14:13:01 -0300

    Meu primeiro post: Alô, mundo!

--- source/pelicanconf.py
+++ source/pelicanconf.py
@@ -12,11 +12,19 @@ DEFAULT_LANG = u'pt'
 # Blogroll
 LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
           ('Python.org', 'http://python.org'),
-          ('Jinja2', 'http://jinja.pocoo.org'),
-          ('You can modify those links in your config file', '#'),)
+          ('Jinja2', 'http://jinja.pocoo.org'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = ()
 
 DEFAULT_PAGINATION = False
+
+ARTICLE_URL = '{lang}/{date:%Y}/{date:%m}/{slug}.html'
+ARTICLE_LANG_URL = '{lang}/{date:%Y}/{date:%m}/{slug}.html'
+PAGE_URL = 'pages/{lang}/{date:%Y}/{date:%m}/{slug}.html'
+PAGE_LANG_URL = 'pages/{lang}/{date:%Y}/{date:%m}/pages/{slug}.html'
+ARTICLE_SAVE_AS = '{lang}/{date:%Y}/{date:%m}/{slug}.html'
+ARTICLE_LANG_SAVE_AS = '{lang}/{date:%Y}/{date:%m}/{slug}.html'
+PAGE_SAVE_AS = 'pages/{lang}/{date:%Y}/{date:%m}/{slug}.html'
+PAGE_LANG_SAVE_AS = 'pages/{lang}/{date:%Y}/{date:%m}/{slug}.html'
+
